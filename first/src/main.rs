c7940a5e732e5a68868b7a51ecb6b3161be38d4c
Silv <forenks1005@gmail.com>
2024-01-22 00:48:13 -0500

    appending

---
+++ first/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+//cargo new - creates new project
+//cargo build - compiles the project
+//cargo run - compiles + runs the project
+//to run the project manually go to target/debug then run the .\projectname.exe 
+//cargo check - checks the health of the project and checks if the code is able to compile w/o compiling
