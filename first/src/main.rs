3bf769ae4b3956ba9352bf52f672c61bb1cf6a88
Silv <forenks1005@gmail.com>
2024-03-01 05:36:57 -0500

    a

--- first/src/main.rs
+++ first/src/main.rs
@@ -7,3 +7,5 @@ fn main() {
 //cargo run - compiles + runs the project
 //to run the project manually go to target/debug then run the .\projectname.exe 
 //cargo check - checks the health of the project and checks if the code is able to compile w/o compiling
+//to indent the code you go src/ and then run the command rustfmt filename.rs
+//both main.rs and the main function are crucial for the code to run
